c1462753dcd1e756d0448b646f80483c376ccb5d
Andriy <atsyperdyuk@gmail.com>
2018-10-25 22:39:43 +0300

    checkpoint

---
+++ Xgame.Model/QuestionRepresentModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xgame.Model
+{
+    public class QuestionRepresentModel
+    {
+        public string QuestionText { get; set; }
+        public string AnswerText { get; set; }
+        // public string QuestionImage { get; set; }
+        //  public string AnswerImage { get; set; }
+    }
+}
