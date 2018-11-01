8d7a88722d2e10d1ffde6edd18477746e625965f
Andriy <atsyperdyuk@gmail.com>
2018-11-01 21:53:57 +0200

    added roles

---
+++ Xgame.Mvc/Controllers/RolesController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xgame.Db.Entities;
+using Xgame.Model;
+
+namespace Xgame.Mvc.Controllers
+{
+    public class RolesController : Controller
+    {
+        RoleManager<IdentityRole> _roleManager;
+        UserManager<AppUser> _userManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index() => View(_roleManager.Roles.ToList());
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(name);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                IdentityResult result = await _roleManager.DeleteAsync(role);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult UserList() => View(_userManager.Users.ToList());
+
+        public async Task<IActionResult> Edit(string userId)
+        {
+            // get list of users
+            AppUser user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                // get list of roles
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var allRoles = _roleManager.Roles.ToList();
+                ChangeRoleViewModel model = new ChangeRoleViewModel
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName,
+                    UserRoles = userRoles,
+                    AllRoles = allRoles
+                };
+                return View(model);
+            }
+
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(string userId, List<string> roles)
+        {
+            // get a user
+            AppUser user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                // get list of roles of user
+                var userRoles = await _userManager.GetRolesAsync(user);
+                // get all roles
+                var allRoles = _roleManager.Roles.ToList();
+                // get list of roles that was added 
+                var addedRoles = roles.Except(userRoles);
+                // get list of roles that was removed
+                var removedRoles = userRoles.Except(roles);
+
+                await _userManager.AddToRolesAsync(user, addedRoles);
+
+                await _userManager.RemoveFromRolesAsync(user, removedRoles);
+
+                return RedirectToAction("UserList");
+            }
+
+            return NotFound();
+        }
+    }
+}
+
